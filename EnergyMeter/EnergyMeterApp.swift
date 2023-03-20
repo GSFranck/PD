08486b896137614c851022b6b31d9b8a3cbdd704
Gustav Franck <gustav.franck@live.dk>
2023-03-20 10:15:19 +0100

    Initial Commit

---
+++ EnergyMeter/EnergyMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyMeterApp.swift
+//  EnergyMeter
+//
+//  Created by Gustav Franck on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EnergyMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
